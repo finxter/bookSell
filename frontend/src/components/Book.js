e35dacc69238b5c61e74df28b7f381999197fb3b
yassesh <yassin2780@gmail.com>
2023-01-06 02:40:58 +0600

    final touch done

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -61,12 +61,14 @@ const Book = ({ state }) => {
           />
         </div>
 
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Subscribe
-        </button>
+        <div className="flex items-center justify-center">
+          <button
+            type="submit"
+            className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-700 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Subscribe
+          </button>
+        </div>
       </form>
     </div>
   );
